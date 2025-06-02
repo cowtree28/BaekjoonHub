a7a663ba81a38bab1af413208972c316b67dc400
treegus <cowtree28@gmail.com>
2025-06-02 19:52:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+long long int square(int n) {
+	long long int arr[1001] = { 0 };
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+	}
+	return arr[n];
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%lld", square(n));
+}
