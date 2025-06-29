17c2b2ea0d0f6ecbe8a64963d730dbbf77372e52
treegus <cowtree28@gmail.com>
2025-06-29 16:54:19 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char word[101];
+	scanf("%s", word);
+	for (int i = 0; i < strlen(word); i++) {
+		if ('a' <= word[i] && word[i] <= 'z')
+			word[i] += ('A' - 'a');
+		else
+			word[i] -= ('A' - 'a');
+	}
+	printf("%s", word);
+}
