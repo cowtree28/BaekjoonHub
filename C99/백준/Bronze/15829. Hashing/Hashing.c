c3183872c480b520985f65c0fb7239ff4fab6669
treegus <cowtree28@gmail.com>
2025-07-05 19:54:12 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- C99/백준/Bronze/15829. Hashing/Hashing.c
+++ C99/백준/Bronze/15829. Hashing/Hashing.c
@@ -1,25 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 #define r 31
 #define M 1234567891
 
 int main() {
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 	char L[50];
 	for (int i = 0; i < n; i++) {
 		scanf(" %c", &L[i]);
 	}
 	long long int sum = 0;
+	long long int pow = 1;
 	for (int i = 0; i < n; i++) {
-		int temp = L[i] - 'a' + 1;
-		for (int j = 0; j < i; j++) {
-			temp = (temp * r) % M;
-		}
-		sum += temp % M;
+		sum += ((L[i] - 'a' + 1) * pow);
+		sum %= M;
+		pow = (pow * r) % M;
+		
 	}
-	sum = sum % M;
-	printf("%d", sum);
+	printf("%lld", sum);
 
 }
