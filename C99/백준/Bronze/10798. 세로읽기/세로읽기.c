f6c045d2c13525c45dbb23bac80d1aaa6b971736
treegus <cowtree28@gmail.com>
2025-08-09 23:35:15 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10798. 세로읽기/세로읽기.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char word[5][16] = {0};
+	for (int i = 0; i < 5; i++)
+		scanf("%s", word[i]);
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (word[j][i] == '\0')
+				continue;
+			putchar(word[j][i]);
+		}
+	}
+}
