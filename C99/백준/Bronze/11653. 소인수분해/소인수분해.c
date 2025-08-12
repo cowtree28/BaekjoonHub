6b496c1c0d6ca8f36198552bbdfdd842ed97f0c5
treegus <cowtree28@gmail.com>
2025-08-12 17:57:39 +0900

    [Bronze I] Title: 소인수분해, Time: 20 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i++) {
+		while (n % i == 0) {
+			n /= i;
+			printf("%d\n", i);
+		}
+	}
+}
