f833381f1f29faae8851066e4f2f77281714de34
treegus <cowtree28@gmail.com>
2025-08-13 20:10:29 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n1, n2;
+	while (1) {
+		scanf("%d %d", &n1, &n2);
+		if (!n1 && !n2)
+			break;
+		if (n2 % n1 == 0)
+			printf("factor\n");
+		else if (n1 % n2 == 0)
+			printf("multiple\n");
+		else
+			printf("neither\n");
+	}
+}
