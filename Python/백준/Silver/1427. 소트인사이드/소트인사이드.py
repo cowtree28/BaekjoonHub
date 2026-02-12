480f709194d8ea5f56d74c91813ab9376b200d87
treegus <cowtree28@gmail.com>
2026-02-12 16:12:34 +0900

    [Silver V] Title: 소트인사이드, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ Python/백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,6 +1,5 @@
 import sys
-input = sys.stdin.readline().strip
-n_str = input()
-arr = list(map(int, [x for x in n_str]))
-arr.sort(reverse=True)
-print("".join(map(str, arr)))
+input = sys.stdin.readline
+n_str = input().strip()
+arr = sorted([x for x in n_str], reverse=True)
+print("".join(arr))
