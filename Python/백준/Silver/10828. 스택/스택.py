01d39fcc0c4f2840b3d47430933f82ea2acd941b
treegus <cowtree28@gmail.com>
2026-02-23 17:10:38 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Silver/10828. 스택/스택.py
+++ Python/백준/Silver/10828. 스택/스택.py
@@ -1,18 +1,17 @@
 import sys
-import collections
 
 n = int(sys.stdin.readline().strip())
-arr = collections.deque()
+stack = []
 
 for _ in range(n):
-    i = sys.stdin.readline().strip().split()
-    if i[0] == "push":
-        arr.append(i[1])
-    elif i[0] == "pop":
-        print(arr.pop() if len(arr) > 0 else -1)
-    elif i[0] == "size":
-        print(len(arr))
-    elif i[0] == "empty":
-        print(int(len(arr) == 0))
-    elif i[0] == "top":
-        print(arr[-1] if len(arr) > 0 else -1)
+    cmd = sys.stdin.readline().strip().split()
+    if cmd[0] == "push":
+        stack.append(cmd[1])
+    elif cmd[0] == "pop":
+        print(stack.pop() if stack else -1)
+    elif cmd[0] == "size":
+        print(len(stack))
+    elif cmd[0] == "empty":
+        print(int(len(stack) == 0))
+    elif cmd[0] == "top":
+        print(stack[-1] if stack else -1)
