e7bba5164b897972ba9d15254ca363c0a4ffed5a
treegus <cowtree28@gmail.com>
2026-02-24 08:49:46 +0900

    [Silver IV] Title: 듣보잡, Time: 76 ms, Memory: 44192 KB -BaekjoonHub

---
+++ Python/백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,10 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+no_listen = set(sys.stdin.readline().strip() for _ in range(n))
+no_see = set(sys.stdin.readline().strip() for _ in range(m))
+
+no_listen_see = sorted(no_listen.intersection(no_see))
+print(len(no_listen_see))
+print(*no_listen_see, sep="\n")
