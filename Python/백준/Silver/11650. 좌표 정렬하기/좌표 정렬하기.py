38c31b70a7305812eadbc946ed352e9e3489a8a6
treegus <cowtree28@gmail.com>
2026-02-23 19:02:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 276 ms, Memory: 46504 KB -BaekjoonHub

--- Python/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ Python/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,7 +1,7 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-arr = [list(map(int, sys.stdin.readline().strip().split())) for x in range(n)]
-arr.sort(key=lambda x: (x[0], x[1]))
+arr = [tuple(map(int, sys.stdin.readline().split())) for x in range(n)]
+arr.sort()
 for x,y in arr:
     print(x,y)
