fd7c5b16af68721ee34c8c00485625f7775c01b4
남우현 <cowtree28@gmail.com>
2026-03-21 21:41:41 +0900

    [Silver V] Title: 숫자 카드, Time: 636 ms, Memory: 116992 KB -BaekjoonHub

--- Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,8 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-arr = set(map(int, sys.stdin.readline().split()))
+arr = set(sys.stdin.readline().split())
 
 n = int(sys.stdin.readline().rstrip())
-for i in list(map(int, sys.stdin.readline().split())):
+for i in sys.stdin.readline().split():
     print(int(i in arr), end=" ")
+
