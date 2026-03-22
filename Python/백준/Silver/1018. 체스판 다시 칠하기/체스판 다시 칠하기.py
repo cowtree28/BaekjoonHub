1e8c8877e2a8c111cd4ddb58fea5e5444124a6ad
남우현 <cowtree28@gmail.com>
2026-03-22 16:58:14 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -9,21 +9,13 @@ min_square = 64
 for i in range(n - 8 + 1):
     for j in range(m - 8 + 1):
         count = 0
+        color = chess[i][j]
         for k in range(8):
             for e in range(8):
                 now = chess[i+k][j+e]
-                if k == 0 and e == 0:
-                    color = now
+                if ((k + e) % 2 == 0) == (color == now):
                     count += 1
-                elif (k * 9 + e) % 2 == 0:
-                    if color == now:
-                        count += 1
-                else:
-                    if color != now:
-                        count += 1
-
-        if count > 32:
-            count = 64 - count
+        count = min(count, 64-count)
         min_square = min(min_square, count)
 
 print(min_square)
