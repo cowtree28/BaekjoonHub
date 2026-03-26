7bb5a5678ac788ca094e41920bc1adb62b5191fa
남우현 <cowtree28@gmail.com>
2026-03-26 09:18:01 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ Python/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,36 +1,26 @@
 import sys
 
+pair = {')':'(', '}':'{', ']':'['}
+
 while True:
     stack = []
     sentence = sys.stdin.readline().rstrip()
     if sentence == ".":
         break
     for i in sentence:
-        if i in ['(', '{', '[']:
+        if i in "({[":
             stack.append(i)
-        elif i in [')', '}', ']']:
+        elif i in ")}]": 
             if not stack:
                 print("no")
                 break
             else:
                 p = stack.pop()
-                match i:
-                    case ')':
-                        if p != '(':
-                            print("no")
-                            break
-                    case '}':
-                        if p != '{':
-                            print("no")
-                            break
-                    case ']':
-                        if p != '[':
-                            print("no")
-                            break
-
+                if pair[i] != p:
+                    print("no")
+                    break
     else:
         if stack:
             print("no")
         else:
             print("yes")
-
